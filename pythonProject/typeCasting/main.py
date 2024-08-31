e9c3d16f4ca3b04111b95baeb8b835c469f1a7b3
veljko146 <veljko146@gmail.com>
2024-08-31 18:00:23 +0200

    typeCasting

---
+++ pythonProject/typeCasting/main.py
@@ -0,0 +1,20 @@
+#Typecasting - the process of converting a variable from one data type to another 
+
+
+name ="Veljko Kovinic"
+age = 23 
+average_grade = 3.2
+is_student = False
+
+#print(type(variable)) prints the type of a variable
+
+average_grade = int(average_grade)
+print(average_grade)
+
+age=str(age)
+print(age)
+print(type(age))
+
+age = float(age)
+print(age)
+
