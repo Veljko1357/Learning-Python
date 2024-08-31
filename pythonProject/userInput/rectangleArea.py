408fcde51cf239bbfa5971fbb6f9f5f8e90be391
veljko146 <veljko146@gmail.com>
2024-08-31 18:13:25 +0200

    Calculate the area of a rectangle

---
+++ pythonProject/userInput/rectangleArea.py
@@ -0,0 +1,10 @@
+# Calculate the area of a rectangle 
+
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+area = length * width
+
+print(f"The area is {area}cm²")
+
+
+#superscript num+alt+0178 ² 
