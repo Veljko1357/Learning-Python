5531883e6fe5b5df27d8a085a2466009ee2814ce
veljko146 <veljko146@gmail.com>
2024-08-31 19:08:39 +0200

    imprted math functions

---
+++ pythonProject/math/importedFunctions.py
@@ -0,0 +1,13 @@
+import math
+
+print(math.pi) #returns value of py
+print(math.e) #returns value of exponential constant
+
+x = 9.4
+
+#math.sqrt = square root of a number
+#math. ceil = rounds number up 
+#math.floor = rounds number down
+
+result = math.sqrt(x)
+print(result)
