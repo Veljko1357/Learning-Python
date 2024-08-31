7d6aed7241e3eb38131b8a9a9b7383b2d0e2297e
veljko146 <veljko146@gmail.com>
2024-08-31 18:51:43 +0200

    A game of Madlibs

---
+++ pythonProject/madlibs/main.py
@@ -0,0 +1,21 @@
+#Madlibs game 
+#word game where you create a story by filling in blanks with random words
+
+
+title = "The Mysterious Journey"
+print(title)
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective (description)")
+noun2 = input("Enter a noun (person, place, thing): ")
+adjective3 = input("Enter an adjective (description)")
+noun3= input("Enter a noun (person, place, thing): ")
+adjective4 = input("Enter an adjective (description)")
+noun4= input("Enter a noun (person, place, thing): ")
+verb = input("Enter a verb: ")
+
+print(f"On a {adjective1} morning, I found a {noun1} in my backyard.")
+print(f"When I touched it, I was suddenly transported to a {adjective2} {noun2}.")
+print(f"There, I met a {adjective3} {noun3} who gave me a {adjective4} {noun4}.")
+print(f"With it, I was able to {verb} my way back home.")
