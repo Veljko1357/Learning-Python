14cdd7472a0be945fbc2e6085da1afddd36fb060
veljko146 <veljko146@gmail.com>
2024-08-31 18:58:00 +0200

    basic math

---
+++ pythonProject/math/main.py
@@ -0,0 +1,29 @@
+#addition
+friends = 10
+
+# friends = friends + 1
+# can also be written as friends += 1 and works for everything else as well
+# print(friends)
+
+#subtraction
+
+# friends = friends - 2
+# print (friends)
+
+
+#multiplication
+# friends = friends * 3
+# print(friends)
+
+#division 
+# friends = friends / 2
+# print(friends)
+
+#exponents
+
+# friends = friends ** 2
+# print(friends)
+
+#remainder 
+friends = friends % 3
+print(friends)
