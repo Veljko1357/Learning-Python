67562a2328828a415a8ccb0751a268f2705f85bb
veljko146 <veljko146@gmail.com>
2024-08-31 17:49:33 +0200

    variables

---
+++ pythonProject/variables/main.py
@@ -0,0 +1,41 @@
+# This is my first Python Program, starting of with printing 
+
+#print("I like pizza!")
+#print("It's really good!")
+
+#Variables (str, int, float, bool) - variable behaves as if it was the value it contains
+
+#strings
+first_name = "Veljko"
+food = "Pizza"
+email = "veljko146@gmail.com"
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is: {email   }")
+
+#Integers
+age = 24
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class {num_of_students} students")
+
+#Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is ${price}")
+print(f"Your gpa is {gpa}")
+
+
+#Booleans
+is_student = False
+if is_student:
+    print("You are not a student")
+else:
+    print("You are a student")
+
+
