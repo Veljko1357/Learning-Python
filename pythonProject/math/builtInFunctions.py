563a2798ff76e4c83cafda3d70bd36dadfa986c5
veljko146 <veljko146@gmail.com>
2024-08-31 19:07:19 +0200

    basic built in functions

---
+++ pythonProject/math/builtInFunctions.py
@@ -0,0 +1,14 @@
+#round - rounds to the nearest integer 
+#abs - absoulte value of a number 
+#pow (value1, value 2) - result of giving value 2 as power to value 1
+# max and min - largest and lowest value between given numbers
+x = 1.2
+y = 6
+z = 7
+
+# result = round(x)
+# result = abs(y)
+# result = pow(z, 3)
+result = max(x, y , z)
+result = min(x, y, z)
+print(result)
