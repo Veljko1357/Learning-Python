72824344694c6f94d16366b896532b10c339ed1e
veljko146 <veljko146@gmail.com>
2024-09-01 01:18:06 +0200

    Basic python string methods

---
+++ pythonProject/string methods/main.py
@@ -0,0 +1,16 @@
+name = input("Enter your full name: ")
+phone_number = input("Enter your phone number: ")
+# result = len(name) -- returns the length of the string as integer
+# result = name.find(" ") - Returns the first occurence of anything inside of "", returns an integer
+# result = name.rfind("0") -  Same as find but searches in reverse, returns -1 if it cannot find 
+# name.capitalize() - capitalizes the first character of a string
+# name.upper() - makes everything uppercase
+# name.lower() - makes everything lowercase
+# result = name.isdigit() - returns boolean value, it only returns true if everything is digits
+# name.isalpha() - returns bool only if string contains alphabetical characters, if there is a space it returns false
+# result = phone_number.count("-") - counts how many characters are within a string
+# phone_number = phone_number.replace("-", "")
+print(phone_number)
+
+
+# print(help(str)) prints all string methods
